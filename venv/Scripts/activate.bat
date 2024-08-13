38786c8f4430f7ce10278a54e08e62591a36088e
venkat <venkatkarthi770@gmail.com>
2024-08-13 21:07:50 +0530

    new changes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=V:\FSD\Portfolio_projects\Qrcode_generator\QR_Code\venv
+set VIRTUAL_ENV=V:\FSD\Portfolio_projects\Qrcode_generator\QRcode\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
